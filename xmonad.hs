24291a79c4c1253697176a8ea74612a8bc7b6cf0
mgrenonville <mgrenonville@mediarithmics.com>
2016-05-03 15:05:01 +0200

    Add dev and prod workspace

--- xmonad.hs
+++ xmonad.hs
@@ -1,5 +1,9 @@
 import qualified Data.Map as M
 
+import XMonad.Prompt
+import XMonad.Prompt.Ssh
+
+
 import System.IO
 import XMonad
 import XMonad.Layout.Accordion
@@ -8,7 +12,6 @@ import XMonad.Actions.CycleWS
 import XMonad.Actions.GridSelect
 import XMonad.Actions.SpawnOn
 import XMonad.Actions.UpdatePointer
-import XMonad.Config.Azerty
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ICCCMFocus
 import XMonad.Hooks.ManageDocks
@@ -24,14 +27,19 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Actions.Plane
 import qualified XMonad.StackSet as W
 import Graphics.X11.ExtraTypes.XF86
+import XMonad.Prompt
+import XMonad.Prompt.Window
+
+import XMonad.Actions.TagWindows
 
 myNormalBorderColor  = "#dddddd"
 myFocusedBorderColor = "#0000BE"
- 
 
 
-myWorkspaces = ["`","1","2","3","4","5","6","7","8","9","0","-","=",
-                "p`","p1","p2","p3","p4","p5","p6","p7","p8","p9","p0","p-","p="]
+devWorkspaces = ["`","1","2","3","4","5","6","7","8","9","0","-","="]
+prodWorkspaces = ["p`","p1","p2","p3","p4","p5","p6","p7","p8","p9","p0","p-","p="]
+
+myWorkspaces = devWorkspaces ++ prodWorkspaces 
 
 full = noBorders Full
 
@@ -61,16 +69,16 @@ myManageHook = composeAll
     , className =? "jetbrains-idea"           --> doF (W.shift "`" )
     ]
 
-    
 
-
-newKeys x = M.union (M.fromList (myKeys x)) (keys azertyConfig x)
+newKeys x = M.union (M.fromList (myKeys x)) (keys defaultConfig x)
 myKeys conf@(XConfig {XMonad.modMask = modMask}) =
     [ ((modMask .|. shiftMask, xK_l), spawn "xscreensaver-command -select 2 ;  xscreensaver-command -lock")
 -- Allow full screen mode
     , ((modMask, xK_f ), sendMessage ToggleLayout)
     , ((0, xF86XK_HomePage), spawn "~/bin/dump_banshee.sh")
     , ((0, xF86XK_Explorer), spawn "xscreensaver-command -select 1 ; xscreensaver-command -lock")
+    , ((modMask .|. shiftMask, xK_g     ), windowPromptGoto  defaultXPConfig )
+    , ((modMask .|. shiftMask, xK_b     ), windowPromptBring defaultXPConfig)
     , ((0, xF86XK_Mail), spawn "xscreensaver-command -select 2 ; xscreensaver-command -lock")
 -- Return to last workspace
     , ((modMask ,  xK_b ), toggleWS )
@@ -78,7 +86,8 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) =
     , ((modMask  , xK_Right ), nextWS )
    -- , ((0, xF86XK_AudioRaiseVolume),   spawn "amixer set Master 2%+")
     --, ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 2%-")
-    
+    , ((modMask .|. controlMask, xK_s), sshPrompt defaultXPConfig)
+
     , ((modMask              , xK_BackSpace), focusUrgent)
     ]
     ++
@@ -95,22 +104,24 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) =
         | (key, sc) <- zip [xK_z, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
     ++
-    [((m .|. modMask, k), windows $ f i)
-        | (i, k) <- zip (workspaces conf) numQwerty,
+    -- Map Super_R to prod workspaces, see .Xmodmap
+    [((m .|. mod3Mask, k), windows $ f i)
+        | (i, k) <- zip (prodWs(workspaces(conf))) numQwerty,
           (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
     ++
     [((m .|. modMask, k), windows $ f i)
-        | (i, k) <- zip (workspaces conf) numAzerty,
+        | (i, k) <- zip (devWs(workspaces(conf))) numQwerty,
           (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
--- keys between ² and = on an azerty keyboard. 13 worspaces.
-numAzerty = [0xb2,0x26,0xe9,0x22,0x27,0x28,0x28,0xe8,0x5f,0xe7,0xe0,0x29,0x3d]
+          where
+          devWs array = take (div (length array ) 2) array
+          prodWs array = drop (div (length array ) 2) array
 
 -- keys between ` and = on an qwerty keyboard. 13 worspaces.
 numQwerty = [0x60,0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38,0x39,0x30,0x2d,0x3d]
 
 main = do
       xmproc <- spawnPipe "xmobar"
-      xmonad $ withUrgencyHook dzenUrgencyHook { args = ["-bg", "darkgreen", "-xs", "1"] }  $ azertyConfig {
+      xmonad $ withUrgencyHook dzenUrgencyHook { args = ["-bg", "darkgreen", "-xs", "1"] }  $ defaultConfig {
                           manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig,
                           keys = newKeys,
                           modMask = mod4Mask,
